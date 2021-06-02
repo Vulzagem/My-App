6524ecf5460121d4e4d8007c88295a1a2b98cca4
Angel Chuecos <angeld.chuecos@gmail.com>
2021-06-02 12:17:29 -0300

    Agregando Navbar

--- src/App.js
+++ src/App.js
@@ -1,23 +1,30 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
+import * as ReactBootstrap from 'react-bootstrap'
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        <ReactBootstrap.Navbar collapseOnSelect expand="lg" bg="dark" variant="dark">
+  <ReactBootstrap.Navbar.Brand href="#home">My App</ReactBootstrap.Navbar.Brand>
+  <ReactBootstrap.Navbar.Toggle aria-controls="responsive-navbar-nav" />
+  <ReactBootstrap.Navbar.Collapse id="responsive-navbar-nav">
+    <ReactBootstrap.Nav className="mr-auto">
+      <ReactBootstrap.Nav.Link href="#features">Log In</ReactBootstrap.Nav.Link>
+      <ReactBootstrap.Nav.Link href="#pricing">Contacto</ReactBootstrap.Nav.Link>
+      <ReactBootstrap.NavDropdown title="Productos" id="collasible-nav-dropdown">
+        <ReactBootstrap.NavDropdown.Item href="#action/3.1">Producto 1</ReactBootstrap.NavDropdown.Item>
+        <ReactBootstrap.NavDropdown.Item href="#action/3.2">Producto 2</ReactBootstrap.NavDropdown.Item>
+        <ReactBootstrap.NavDropdown.Item href="#action/3.3">Producto 3</ReactBootstrap.NavDropdown.Item>
+        <ReactBootstrap.NavDropdown.Divider />
+        <ReactBootstrap.NavDropdown.Item href="#action/3.4">Producto Premium</ReactBootstrap.NavDropdown.Item>
+      </ReactBootstrap.NavDropdown>
+    </ReactBootstrap.Nav>
+    <ReactBootstrap.Nav>
+      <ReactBootstrap.Nav.Link href="#deets">Mas Detalles</ReactBootstrap.Nav.Link>
+    </ReactBootstrap.Nav>
+  </ReactBootstrap.Navbar.Collapse>
+</ReactBootstrap.Navbar>
     </div>
   );
 }
